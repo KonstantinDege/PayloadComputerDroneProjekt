869ebffda1711808b030cfdd5e30be682ec15c83
Skeletmaster <150815971+KonstantinDege@users.noreply.github.com>
2025-04-09 11:11:21 +0200

    added more mov functions

--- src/payloadcomputerdroneprojekt/communications/__init__.py
+++ src/payloadcomputerdroneprojekt/communications/__init__.py
@@ -2,14 +2,30 @@ class Communications:
     def __init__(self, commlink):
         self.mavsdk = commlink
 
-    def mov_xyz(self, pos, yaw):
+    def mov_to_xyz(self, pos, yaw):
         """
         parms:
-         pos: Postition Vector
+         pos: Postition Vector local
          yaw: Yaw Angle
         """
         pass
-    
+
+    def mov_to_latlonalt(self, pos, yaw):
+        """
+        parms:
+         pos: Postition Vector global
+         yaw: Yaw Angle
+        """
+        pass
+
+    def mov_by_xyz(self, pos, yaw):
+        """
+        parms:
+         pos: Postition Vector local relativ
+         yaw: Yaw Angle
+        """
+        pass
+
     def mov_v(self, v, yaw):
         """
         parms:
@@ -17,53 +33,52 @@ class Communications:
          yaw: Yaw Angle
         """
         pass
-    
+
     def get_position(self):
         """
 
         return [x,y,z,roll,pitch,yaw]
         """
         pass
-        
+
     def send_status(self, status: str):
         """
-        
+
         parms:
          status: Status Message
-         
+
         return:
          ok: bool
         """
         pass
-        
+
     def send_image(self, image):
         """
-        
+
         parms:
          image: Image Message
-         
+
         return:
          ok: bool
         """
         pass
-    
+
     def send_found_obj(self):
         """
-        
+
         parms:
          obj: Object Database
-         
+
         return:
          ok: bool
         """
         pass
-    
+
     def receive_mission_plan(self):
         """
-         
+
         return:
          plan: str<json>
          ok: bool
         """
-        
         pass
