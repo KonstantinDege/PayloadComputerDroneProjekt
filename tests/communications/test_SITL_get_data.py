d93c566e33de75f48b031c24fb9c05764b823703
KonstantinDege <dege.konstantin@gmail.com>
2025-05-01 09:52:38 +0200

    added some tests for communications

---
+++ tests/communications/test_SITL_get_data.py
@@ -0,0 +1,42 @@
+from payloadcomputerdroneprojekt.communications import Communications
+import asyncio
+import unittest
+
+PORT = "udp://:14540"
+
+class CommAsync:
+    @staticmethod
+    async def get_lat_lon_alt():
+        con = Communications(address=PORT)
+        await con.connect()
+        pos = await con.get_position_lat_lon_alt()
+        print(f"LatLonAlt {pos}")
+        
+    @staticmethod
+    async def get_xyz():
+        con = Communications(address=PORT)
+        await con.connect()
+        pos = await con.get_position_xyz()
+        print(f"XYZ {pos}")
+        
+    @staticmethod
+    async def get_vel():
+        con = Communications(address=PORT)
+        await con.connect()
+        pos = await con.get_velocity_xyz()
+        print(f"Velocity {pos}")
+
+
+class TestCommunication(unittest.TestCase):
+    def test_get_lat_lon_alt(self):
+        asyncio.run(CommAsync.get_lat_lon_alt())
+        
+    def test_get_xyz(self):
+        asyncio.run(CommAsync.get_xyz())
+        
+    def test_get_vel(self):
+        asyncio.run(CommAsync.get_vel())
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/communications/test_SITL_mov.py
@@ -0,0 +1,22 @@
+from payloadcomputerdroneprojekt.communications import Communications
+import asyncio
+import unittest
+
+PORT = "udp://:14540"
+
+class CommAsync:
+    @staticmethod
+    async def move_by():
+        con = Communications(address=PORT)
+        await con.connect()
+
+        await con.mov_by_xyz([0,0,-10], 0)
+        
+
+
+class TestCommunication(unittest.TestCase):
+    def test_move_by(self):
+        asyncio.run(CommAsync.move_by())
+
+if __name__ == '__main__':
+    unittest.main()
