aa84b717b6c4076459fc40ab81ec06e3a7cdf535
Sandro Mueller <sam1493@thi.de>
2025-05-02 14:41:28 +0200

    test cases for pixel coordinate and gps coordinate

--- src/payloadcomputerdroneprojekt/image_analysis/math_helper.py
+++ src/payloadcomputerdroneprojekt/image_analysis/math_helper.py
@@ -12,7 +12,7 @@ def compute_pixel_vec(px, py, imagesize, fov):
     x = px/(imagesize[0]/2) - 1
     y = py/(imagesize[1]/2) - 1
 
-    return np.array([x * math.tan(fov[0]), y * math.tan(fov[1]), 1])
+    return np.array([-x * math.tan(fov[0]), y * math.tan(fov[1]), 1])
 
 
 def rotation_matrix(rot):
--- src/payloadcomputerdroneprojekt/test/image_analysis/test_pixel_calc.py
+++ src/payloadcomputerdroneprojekt/test/image_analysis/test_pixel_calc.py
@@ -1,3 +1,4 @@
+import unittest.async_case
 from payloadcomputerdroneprojekt.image_analysis import ImageAnalysis
 import unittest
 import os
@@ -25,6 +26,142 @@ class TestImage(unittest.TestCase):
         pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
         assert np.linalg.norm(np.array(pos) - np.array([0, 0, 1])) == 0
 
+    def test_local_coords_pos_x_positive(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 0,
+            "y_center": 325
+        }
+        pos_com = [
+            0, 0, 0, 0, 0, 0
+        ]
+        height = 1
+        pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
+        assert pos[0] > 0
+        assert pos[1] == 0
+
+    def test_local_coords_pos_y_positive(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 230,
+            "y_center": 600
+        }
+        pos_com = [
+            0, 0, 0, 0, 0, 0
+        ]
+        height = 1
+        pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
+        assert pos[1] > 0
+        assert pos[0] == 0
+
+    def test_local_coords_pos_x_negative(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 460,
+            "y_center": 325
+        }
+        pos_com = [
+            0, 0, 0, 0, 0, 0
+        ]
+        height = 1
+        pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
+        assert pos[0] < 0
+        assert pos[1] == 0
+
+    def test_local_coords_pos_y_negative(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 230,
+            "y_center": 0
+        }
+        pos_com = [
+            0, 0, 0, 0, 0, 0
+        ]
+        height = 1
+        pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
+        assert pos[1] < 0
+        assert pos[0] == 0
+
+    def test_local_coords_pos(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 230,
+            "y_center": 325
+        }
+        pos_com = [
+            0, 0, 0, 0, 0, 0
+        ]
+        height = 1
+        pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
+        assert pos[1] == 0
+        assert pos[0] == 0
+
+    def test_local_coords_pitch_angle(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 230,
+            "y_center": 325
+        }
+        pos_com = [
+            0, 0, 0, 0, 10, 0
+        ]
+        height = 1
+        pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
+
+        self.assertAlmostEqual(
+            pos[0], 0 + height * np.tan(np.deg2rad(pos_com[4])), delta=0.0001)
+        assert pos[1] == 0
+
+    def test_local_coords_roll_angle(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 230,
+            "y_center": 325
+        }
+        pos_com = [
+            0, 0, 0, 5, 0, 0
+        ]
+        height = 1
+        pos = ia.add_latlonalt(obj, pos_com, height, (460, 650))
+        assert pos[0] == 0
+        self.assertAlmostEqual(
+            pos[1], 0 - height * np.tan(np.deg2rad(pos_com[3])), delta=0.0001)
+        
 
 if __name__ == '__main__':
     unittest.main()
