774918f675daa6947b3cb756aa20180fe4c01997
Sandro Mueller <sam1493@thi.de>
2025-05-14 15:05:08 +0200

    thomas testst

--- src/payloadcomputerdroneprojekt/test/image_analysis/test_pixel_calc.py
+++ src/payloadcomputerdroneprojekt/test/image_analysis/test_pixel_calc.py
@@ -161,7 +161,197 @@ class TestImage(unittest.TestCase):
         assert pos[0] == 0
         self.assertAlmostEqual(
             pos[1], 0 - height * np.tan(np.deg2rad(pos_com[3])), delta=0.0001)
-        
+
+    def test_local_coords_pos2_0_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 176,
+            "y_center": 124
+        }
+        pos_com = [
+            0, 0, 0, 2.5, 0, 0
+        ]
+        height = 0.83
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == -0.06
+        assert pos[1] == -0.16
+
+    def test_local_coords_pos3_0_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 299,
+            "y_center": 391
+        }
+        pos_com = [
+            0, 0, 0, -4, 0, 0
+        ]
+        height = 0.83
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == -0.065
+        assert pos[1] == 0.065
+
+    def test_local_coords_pos1_10_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 407,
+            "y_center": 319
+        }
+        pos_com = [
+            0, 0, 0, 0, 10, 0
+        ]
+        height = 0.83
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == 0.15
+        assert pos[1] == 0
+
+    def test_local_coords_pos2_10_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 182,
+            "y_center": 348
+        }
+        pos_com = [
+            0, 0, 0, 0, 10, 0
+        ]
+        height = 0.83
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == 0.05
+        assert pos[1] == 0.03
+
+    def test_local_coords_pos3_10_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 297,
+            "y_center": 255
+        }
+        pos_com = [
+            0, 0, 0, 2.5, 10, 0
+        ]
+        height = 0.83
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == 0.055
+        assert pos[1] == 0.05
+
+    def test_local_coords_pos4_10_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 369,
+            "y_center": 425
+        }
+        pos_com = [
+            0, 0, 0, 2.5, 10, 0
+        ]
+        height = 0.83
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == -0.115
+        assert pos[1] == 0.1
+
+    def test_local_coords_pos1_30_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 360,
+            "y_center": 319
+        }
+        pos_com = [
+            0, 0, 0, 0, 30, 0
+        ]
+        height = 0.84
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == -0.15
+        assert pos[1] == 0
+
+    def test_local_coords_pos2_30_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 220,
+            "y_center": 225
+        }
+        pos_com = [
+            0, 0, 0, 2.5, 30, 0
+        ]
+        height = 0.84
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == 0.02
+        assert pos[1] == -0.09
+
+    def test_local_coords_pos3_30_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 289,
+            "y_center": 367
+        }
+        pos_com = [
+            0, 0, 0, 2.5, 30, 0
+        ]
+        height = 0.84
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == -0.055
+        assert pos[1] == -0.055
+
+    def test_local_coords_pos4_30_degree(self):
+        with open(os.path.join(FILE_PATH, "test_config.json")) as json_data:
+            config = json.load(json_data)["image"]
+
+        config["path"] = "."
+        ia = ImageAnalysis(config, "", "")
+
+        obj = {
+            "x_center": 342,
+            "y_center": 375
+        }
+        pos_com = [
+            0, 0, 0, 2.5, 30, 0
+        ]
+        height = 0.84
+        pos = ia.add_latlonalt(obj, pos_com, height, (480, 640))
+        assert pos[0] == -0.12
+        assert pos[1] == -0.06
+
 
 if __name__ == '__main__':
     unittest.main()
