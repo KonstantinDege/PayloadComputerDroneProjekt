01d7b51b58b8f042b1cf22907d6bc9eb770de31a
KonstantinDege <dege.konstantin@gmail.com>
2025-05-18 13:50:11 +0200

    added docstrings to offboard elements of the comms

--- src/payloadcomputerdroneprojekt/communications/__init__.py
+++ src/payloadcomputerdroneprojekt/communications/__init__.py
@@ -54,17 +54,28 @@ class Communications:
                        lambda x: x.is_connected)
         print("-- Connection established successfully")
 
-    async def check_health(self):
+    async def check_health(self) -> bool:
+        """Return if GPS Position is Okay
+
+        Returns:
+            bool:
+        """
         return (await get_data(self.drone.telemetry.health())
                 ).is_global_position_ok
 
     async def wait_for_health(self):
+        """
+        Runs unit the home position and global position is okay.
+        """
         await wait_for(
             self.drone.telemetry.health(),
             lambda x: x.is_global_position_ok and x.is_home_position_ok)
 
     @save_execute("Arm")
     async def await_arm(self):
+        """
+        Arms or wait until the drone is armed
+        """
         if self.config.get("allowed_arm", False):
             try:
                 await self.drone.action.arm()
@@ -75,6 +86,9 @@ class Communications:
 
     @save_execute("Disarm")
     async def await_disarm(self):
+        """
+        Disarms or wait until the drone is disarmed
+        """
         if self.config.get("allowed_disarm", False):
             try:
                 await self.drone.action.disarm()
@@ -99,18 +113,39 @@ class Communications:
                 await wait_for(self.drone.telemetry.flight_mode(),
                                lambda x: x == "OFFBOARD")
 
-    async def get_relative_height(self):
+    async def get_relative_height(self) -> float:
+        """
+        Get height estimation to the ground
+        Returns:
+            _type_: _description_
+        """
         if not await self.check_health():
             print("Telemetry not ready")
-            return -5
+            return -5.
         return (await get_data(self.drone.telemetry.position()
                                )).relative_altitude_m
 
-    async def is_flighing(self):
+    async def is_flighing(self) -> bool:
+        """
+        Checks if drone is currently flighing
+
+        Returns:
+            bool:
+        """
         return await get_data(self.drone.telemetry.in_air())
 
     @save_execute("Start")
     async def start(self, height: float = 5):
+        """
+        Starts the drone and run it until it reached the target height or is
+        heigher
+
+        Args:
+            height (float, optional): Height over the ground. Defaults to 5.
+
+        Returns:
+            _type_: _description_
+        """
         await self.await_arm()
         if await self.get_relative_height() >= height:
             return True
@@ -133,7 +168,14 @@ class Communications:
             return [0, 0, 0]
         return (await get_data(self.drone.telemetry.attitude_euler())).yaw_deg
 
-    async def get_position_xyz(self):
+    async def get_position_xyz(self) -> list[float]:
+        """
+        Returns list of floating point values. If the gps is not ready it will
+        return zeros but not wait until it is ready.
+
+        Returns:
+            list[float]: x, y, z, roll, pitch, yaw
+        """
         if not await self.check_health():
             print("Telemetry not ready")
             return [0, 0, 0, 0, 0, 0]
@@ -142,6 +184,13 @@ class Communications:
         return get_pos_vec(state) + await self._get_attitude()
 
     async def get_position_lat_lon_alt(self):
+        """
+        Returns list of floating point values. If the gps is not ready it will
+        return zeros but not wait until it is ready.
+        [m, degree]
+        Returns:
+            list[float]: lat, lon, alt, roll, pitch, yaw
+        """
         if not await self.check_health():
             print("Telemetry not ready")
             return [0, 0, 0, 0, 0, 0]
@@ -150,7 +199,15 @@ class Communications:
                 ] + await self._get_attitude()
 
     @save_execute("Move to XYZ")
-    async def mov_to_xyz(self, pos, yaw=None):
+    async def mov_to_xyz(self, pos: list[float], yaw: float = None):
+        """
+        Move to the XYZ coordinates, XYZ is fixed to the start position and yaw
+        0 is North.
+
+        Args:
+            pos (list[float]): XYZ [m] coordinates in local ned values
+            yaw (float, optional): target yaw [degree] in compass values
+        """
         if yaw is None:
             yaw = await self._get_yaw()
         await self.drone.offboard.set_position_ned(PositionNedYaw(
@@ -161,20 +218,44 @@ class Communications:
                         self.config.get("vel_error", 0.5)))
 
     @save_execute("Move with Velocity")
-    async def mov_with_vel(self, vel, yaw=None):
+    async def mov_with_vel(self, vel: list[float], yaw: float = None):
+        """
+        Move the drone with a fixed velocity in XYZ direction [m/s].
+        XYZ in global position system
+
+        Args:
+            vel (list[float]):
+            yaw (float, optional): Realtiv to North [degree]
+        """
         if yaw is None:
             yaw = await self._get_yaw()
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(
             north_m_s=vel[0], east_m_s=vel[1], down_m_s=vel[2], yaw_deg=yaw))
 
     @save_execute("Move by Velocity")
-    async def mov_by_vel(self, vel, yaw=0):
+    async def mov_by_vel(self, vel: list[float], yaw: float = 0):
+        """
+        Move the drone rotated to drone yaw coordinates
+
+        Args:
+            vel (list[float]): Velocity orientated to drone yaw
+            yaw (float, optional):  Adds the
+                yaw to the current one. Defaults to 0.
+        """
         old_yaw = await self._get_yaw()
         new_vel = rotation_matrix_yaw(old_yaw) @ np.array(vel)
         await self.mov_with_vel(new_vel[0], old_yaw + yaw)
 
     @save_execute("Move by XYZ")
-    async def mov_by_xyz(self, pos, yaw=0):
+    async def mov_by_xyz(self, pos: list[float], yaw: float = 0):
+        """
+        Move the drone by XYZ in local drone yaw coordinates
+
+        Args:
+            pos(list[float]):
+            yaw(float, optional): Adds the
+                yaw to the current one. Defaults to 0.
+        """
         pos = np.array(pos)
         cur_pos = await self.get_position_xyz()
         yaw_cur = cur_pos[5]
@@ -184,7 +265,14 @@ class Communications:
         await self.mov_to_xyz(new_pos[0], yaw)
 
     @save_execute("Move by XYZ old")
-    async def mov_by_xyz_old(self, pos, yaw=0):
+    async def mov_by_xyz_old(self, pos: list[float], yaw: float = 0):
+        """
+        Move the drone by XYZ in local reference frame. Yaw = 0 => North
+
+        Args:
+            pos(g):
+            yaw(float, optional): Sets the new yaw as target. Defaults to 0.
+        """
         pos = np.array(pos)
         cur_pos = await self.get_position_xyz()
         yaw += cur_pos[5]
@@ -193,7 +281,16 @@ class Communications:
         await self.mov_to_xyz(new_pos, yaw)
 
     @save_execute("Move to Lat Lon Alt")
-    async def mov_to_lat_lon_alt(self, pos, yaw=None):
+    async def mov_to_lat_lon_alt(self, pos: list[float], yaw: float = None):
+        """
+        Move the drone to the lat lon global coordinates with a relativ height
+        and a target yaw.
+
+        Args:
+            pos (list[float]): [lat, lon, delta_h] [degree, degree, m]
+            yaw (float, optional): [degree]. Defaults to current.
+
+        """
         if yaw is None:
             yaw = await self._get_yaw()
         await self.drone.offboard.set_position_global(PositionGlobalYaw(
@@ -215,6 +312,9 @@ class Communications:
 
     @save_execute("Land")
     async def land(self):
+        """
+        Trigger standard land mode of the drone.
+        """
         await self.drone.action.land()
 
     def receive_mission_file():
