e1c3ae60db18fa3739e0eb816e1be80ee96b612f
muellersandro <sam1493@thi.de>
2025-06-18 17:51:29 +0200

    dev2

--- src/payloadcomputerdroneprojekt/__init__.py
+++ src/payloadcomputerdroneprojekt/__init__.py
@@ -1,4 +1,4 @@
-from payloadcomputerdroneprojekt.mission_computer import \
+from payloadcomputerdroneprojekt.mission_computer.mc_class import \
     MissionComputer  # noqa: F401
 
 __version__ = "0.1.2"
